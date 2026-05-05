2df0a1fe85fd569cec695b2b7f095284b948b2ed
Nicole Soloveichik <nicole.s@sealsecurity.io>
2026-05-05 21:45:59 +0300

    apply 66614

--- java/org/apache/coyote/http2/Http2UpgradeHandler.java
+++ java/org/apache/coyote/http2/Http2UpgradeHandler.java
@@ -1925,6 +1925,10 @@ class Http2UpgradeHandler extends AbstractStream implements InternalHttpUpgradeH
     }
 
 
+    String getSniHostName() {
+        return socketWrapper.getSniHostName();
+    }
+
     protected class PingManager {
 
         protected boolean initiateDisabled = false;
